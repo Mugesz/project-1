d1cab72f7b2e477dc8a90af84f7e56deb97695e0
frekerzzzz <kannamugesh135@gmail.com>
2024-09-25 12:47:24 +0530

    kergbs

--- i.js
+++ i.js
@@ -12,10 +12,34 @@
 //     console.log("Enter a valid Number")
 // }
 
-function convert() {
-  const input = document.getElementById("input").value;
-  let inch = input  * (9/5) + 32;
+// function convert() {
+//   const input = document.getElementById("input").value;
+//   let inch = input  * (9/5) + 32;
 
-  const result = document.getElementById("result");
-  result.innerHTML = inch.toFixed(2) + " "+"farenheat";
-}
+//   const result = document.getElementById("result");
+//   result.innerHTML = inch.toFixed(2) + " "+"farenheat";
+// }
+
+
+// let str = "If you're visiting this page"
+
+// let str1 = "it can also be a fun way to surprise others."
+
+// console.log(str.concat(str1));
+
+// console.log(str.concat(" ",str1));
+
+// console.log(str.substr(8,16))
+
+// console.log(str1.replaceAll('i','t'))
+
+
+
+
+// PRINT RANDOM STRING
+
+// let value = "Mugesh";
+
+// let randomLetter = value.charAt(Math.floor(Math.random() * value.length));
+
+// console.log(randomLetter);
