6271bc3c792f7621a26a16475f29d0fa65f1247f
frekerzzzz <kannamugesh135@gmail.com>
2024-09-25 18:34:31 +0530

    h

--- i.js
+++ i.js
@@ -40,11 +40,7 @@
 
 // console.log(randomLetter);
 
-
-
 // TEMPLAE LITERALS
-
-
 
 // let Fn = "mugesh";
 // let Ln = "kanna";
@@ -53,7 +49,7 @@
 // let combine = Fn + " " + Ln + " " + "Lives In " + city;
 // let msg = `${Fn} ${Ln} Lives In ${city}`;
 
-// console.log(combine); 
+// console.log(combine);
 // console.log(msg);
 
 // let items = 5;
@@ -65,29 +61,57 @@
 
 // ARRAYS
 
-  // let arr = [1,2,3,4,5,6,7,8,9]
-  // console.log(arr.length);
-  // console.log(arr[8])
-  // console.log(arr[arr.length-1])
+// let arr = [1,2,3,4,5,6,7,8,9]
+// console.log(arr.length);
+// console.log(arr[8])
+// console.log(arr[arr.length-1])
 
-  // let array = [5, 7, 6, [1,2], "hello"];
-  // // console.log(array[3][0])
-  // console.log(array[4])
+// let array = [5, 7, 6, [1,2], "hello"];
+// // console.log(array[3][0])
+// console.log(array[4])
 
-  let arr = ["a","c","e"]
+// let arr = ["a","c","e","f"];
 
-  // arr.push('t')   //add last element in array
-  // console.log(arr)
+// arr.push('t')   //add last element in array
+// console.log(arr)
 
-  // arr.pop()  // remove the last element in array
-  // console.log(arr)
+// arr.pop()  // remove the last element in array
+// console.log(arr)
 
-  // arr.shift()  // remove first element from array
-  // console.log(arr)
+// arr.shift()  // remove first element from array
+// console.log(arr)
 
-  // arr.unshift("g"); //add first element in array
-  // console.log(arr)
+// arr.unshift("g"); //add first element in array
+// console.log(arr)
 
-  arr.splice(2,0,'k') //add or remove or replace  the elements whereever you want with the help of index values
-  console.log(arr);
-  
+// arr.splice(2,0,'k') //add or remove or replace  the elements whereever you want with the help of index values
+// console.log(arr);
+
+// console.log(arr.slice(-2)); //starting and ending index last index value is not included
+
+// console.log( arr.reverse()) // reversing the array permenently
+
+//  let str =  arr.join()
+//  console.log(str); // convert array to string
+
+// let string = "g,t,b,e";
+// let array = string.split(",")  // convert string to array;
+// console.log(array);
+
+// let yak = [1, 2, 3];
+// let dho = [4, 5, 6];
+
+// // let joins = yak.concat(dho); // join two array in single arrazy
+
+// // let joins = [...yak,...dho];  // spread operator
+// console.log(joins);
+
+
+// Arr=[5,8,10,7,9,11];
+
+// let ans = Arr.splice(3,3,17,19,11)  // replacing array
+//  Arr.unshift(100) // insert 100 in the beginning
+// Arr.push(1000) //add 1000 at last
+// Arr.splice(3,0,0);
+
+// console.log(Arr)
